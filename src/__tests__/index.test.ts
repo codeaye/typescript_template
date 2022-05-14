171dc31c3579a921b0bdc745975b8c5520c455c8
Nav <62173713+codeaye@users.noreply.github.com>
2022-05-14 22:43:07 +0100

    feat: add standard-version
    Add standard versioning and better workflow with ci!

--- src/__tests__/index.test.ts
+++ src/__tests__/index.test.ts
@@ -3,3 +3,7 @@ import add from "../index";
 test("Add 2 and 2 and expect it to be 4", () => {
 	expect(add(2, 2)).toBe(4);
 });
+
+test("Add 4 and 4 and expect it not to be 4", () => {
+	expect(add(4, 4)).not.toBe(4);
+});
